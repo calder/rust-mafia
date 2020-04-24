4cc0bb6f356c8d18baa77c3fcd7f676a0bd0fd65
Calder Coalson <caldercoalson@gmail.com>
2020-04-24 06:01:38 -0700

    Add Serde.

--- mafia/src/game.rs
+++ mafia/src/game.rs
@@ -1,78 +1,122 @@
+use serde::{Deserialize, Serialize};
+
 type Map<K, V> = std::collections::BTreeMap<K, V>;
 type Set<T> = std::collections::BTreeSet<T>;
 
 type Player = String;
 
-struct Game {
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Game {
     players: Set<Player>,
     start: State,
     state: State,
     log: Log,
 }
 
-struct State {
+impl Game {
+    fn new() -> Self {
+        Game {
+            players: Set::new(),
+            start: State::new(),
+            state: State::new(),
+            log: Log::new(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct State {
     players: Map<Player, PlayerState>,
     phase: Phase,
 }
 
-struct PlayerState {
+impl State {
+    fn new() -> Self {
+        State {
+            players: Map::new(),
+            phase: Phase::Night(0),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PlayerState {
     modifiers: Vec<Modifier>,
 }
 
-struct Modifier {
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Modifier {
     effect: Effect,
     expiration: Deadline,
 }
 
-enum Effect {
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Effect {
     Has(Ability),
     Dead,
     Poisoned(Deadline),
     Protected,
 }
 
-enum Deadline {
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Deadline {
     Never,
     Days(i64),
     Nights(i64),
 }
 
-enum Phase {
-    Day,
-    Night,
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Phase {
+    Day(i64),
+    Night(i64),
 }
 
-struct Log {
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Log {
     events: Vec<Event>,
 }
 
-enum Event {
+impl Log {
+    fn new() -> Self {
+        Log { events: Vec::new() }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Event {
     Planned(Player, Action),
     Used(Player, Action),
     Died(Player),
 }
 
-enum Action {
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Action {
     Investigate(Player),
     Kill(Player),
     Protect(Player),
 }
 
-enum Ability {
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Ability {
     Investigate,
     Kill,
     Protect,
 }
 
-enum Command {
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Command {
     Plan(Action),
     Use(Action),
 }
 
 #[cfg(test)]
 mod test {
+    use super::*;
+
     #[test]
     fn placeholder() {
+        let game = Game::new();
+        println!("{:?}", ron::ser::to_string(&game));
         assert!(true);
     }
 }
