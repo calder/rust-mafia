0313085840725d6a474d13ad95a257a1579722ce
Calder Coalson <caldercoalson@gmail.com>
2020-04-26 11:59:18 -0700

    Autogenerate tests.

--- mafia/tests/test.rs
+++ mafia/tests/test.rs
@@ -1,16 +1,3 @@
 mod util;
 
-#[test]
-fn test_basic_game() {
-    util::run_test("test_basic_game")
-}
-
-#[test]
-fn test_mafia_win() {
-    util::run_test("test_mafia_win")
-}
-
-#[test]
-fn test_town_win() {
-    util::run_test("test_town_win")
-}
+include!(concat!(env!("OUT_DIR"), "/generated_tests.rs"));
