25cd93839639bd4403659e767487824cb4d4a2b7
Calder Coalson <caldercoalson@gmail.com>
2020-04-29 17:38:51 -0700

    Remove server test in preparation for cleaner test framework.

--- mafia-bin/tests/test_server.rs
+++ mafia-bin/tests/test_server.rs
@@ -1,4 +1,3 @@
-extern crate lazy_static;
 extern crate log;
 
 mod util;
@@ -6,9 +5,4 @@ mod util;
 #[tokio::test]
 async fn test_server_smoketest() {
     util::mafia(&["host", "--smoketest"]).await.unwrap();
-}
-
-#[tokio::test]
-async fn test_server_hello() {
-    util::run_server_test("test_server_hello").await;
 }
